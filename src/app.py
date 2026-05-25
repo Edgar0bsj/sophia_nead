79fbe03b75211d40226e4f792204e9d4a1fbe231
EAD UNIG <unig.ead@campus1.unig.br>
2026-05-25 16:48:34 -0300

    ajustes feito na interação do usuario e path do arquivo base feito com sucesso!

--- src/app.py
+++ src/app.py
@@ -4,51 +4,11 @@ from src.controllers.polo_controller import PoloController
 from src.schemas.polo_schema import PoloInput
 
 
+
 class MainApp:
-    def __init__(self):
-        self.tables = Tables()
-        self.polo_controller = PoloController()
-        
-        self.tables.create_tables()
-        
-    
-        
-    def inicializar(self):
+        pass
 
-# ///////////////////////////////////////////////////
-#                      CREATE POLO
-# ///////////////////////////////////////////////////
-        polo = PoloInput(nome='ItaperuNAN')
-        #>>>> self.polo_controller.create_polo(polo)
-        
-# ///////////////////////////////////////////////////
-#                      FIND ALL POLO
-# ///////////////////////////////////////////////////
-        poloss = self.polo_controller.find_all_polo()
-        
-# ///////////////////////////////////////////////////
-#                      FIND BY ID POLO
-# ///////////////////////////////////////////////////
-        self.polo_controller.find_by_id_polo(poloss[0].id)
-        
-# ///////////////////////////////////////////////////
-#                      EDIT POLO
-# ///////////////////////////////////////////////////
-        print(self.polo_controller.update_polo(PoloInput(
-            id=poloss[1].id,
-            nome="Catabolas"
-        )))
-        
-# ///////////////////////////////////////////////////
-#                      DELETE
-# ///////////////////////////////////////////////////
-        self.polo_controller.delete_polo(poloss[0].id)
-
-# ///////////////////////////////////////////////////
-#                   INICIALIZAÇÃO
-# ///////////////////////////////////////////////////
 if __name__ == "__main__":
-    app = MainApp()
-    app.inicializar()
+    pass
     
     
